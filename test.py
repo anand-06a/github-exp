ef1654a8e0824078ff77935538f7e9e8ed1b43d9
anand-06a <anandralikatti6@gmail.com>
2026-07-21 15:35:24 +0530

    modified test.py

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print(5)
+a=7,'p',9
+print(type(a))
+
+# try to find out vowel from sentence
+sentence = 'Hello, how are you?'
+vowels = "aeiouAEIOU"
+
+for letter in sentence:
+    if letter in vowels:
+        print(letter)
+
+
+sentence = "Hello, how are you?"
+vowels = [letter for letter in sentence if letter.lower() in "aeiouAEIOU"]
+print(vowels)        
